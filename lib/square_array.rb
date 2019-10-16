cbbdd51f58b0b859a8e977f145117d3a531426e3
Conrad Robinson <robinsonc220@gmail.com>
2019-10-16 19:52:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,8 @@
 def square_array(array)
-  array = [2,4,6]
-  puts array.each { |i| i ** 2 }
+  counter = 0
+  array = [2, 4, 6]
+  while array[counter] do
+    puts array[counter] ** 2
+    counter += 1
+  end
 end
